dcdb413928dd9212c58ba18a932d26cc637ceef0
阮传宙 <ruanchuanzhou@interlib.vip>
2021-09-09 00:48:51 +0800

    feat: node可以运行起来了

--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
-import koa from 'koa'
+import Koa from 'koa'
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser'
 
-console.log(koa)
+const app = new Koa()
+
+const router = new Router()
+router.get('/', (cxt) => {
+  cxt.body = 'hello'
+})
+app.use(router.routes()).use(
+  bodyParser({
+    enableTypes: ['json', 'form', 'text']
+  })
+)
+
+app.listen(3001)
